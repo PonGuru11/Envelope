4a955bd30eadff72f1352d01643404fae9cc2e05
PonGuru11 <125974004+PonGuru11@users.noreply.github.com>
2023-04-28 19:29:24 +0530

    Update envelopesController.js

--- controllers/envelopesController.js
+++ controllers/envelopesController.js
@@ -1,8 +1,8 @@
 const envelopesService = require('../services/envelopesService.js');
 exports.createEnvelope = async (req, res) => {
   try {
-    const { filePath, email, name } = req.body;
-    const envelope = await envelopesService.createEnvelope(filePath, email, name);
+    const { documentUrl, email, name } = req.body;
+    const envelope = await envelopesService.createEnvelope(documentUrl, email, name);
   
     res.status(201).json(envelope);
   } catch (err) {
